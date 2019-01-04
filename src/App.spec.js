5c6c9cd254579a4d4f43893fdf5d2a2dfed00562
serhijko <serhijko>
2019-01-04 13:32:11 +0300

    Unit Tests for React State Changes

---
+++ src/App.spec.js
@@ -0,0 +1,17 @@
+import { doIncrement, doDecrement } from './App';
+
+describe('Local state', () => {
+  it('should increment the counter in state', () => {
+    const state = { counter: 0 };
+    const newState = doIncrement(state);
+
+    expect(newState.counter).to.equal(1);
+  });
+
+  it('should decrement the counter in state', () => {
+    const state = { counter: 0 };
+    const newState = doDecrement(state);
+
+    expect(newState.counter).to.equal(-1);
+  });
+});
